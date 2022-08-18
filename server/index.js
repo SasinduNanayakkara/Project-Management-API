e96639f385684a3dc66c60aa474acd05a34701a6
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-08-18 15:38:14 +0530

    Connect to the db

--- server/index.js
+++ server/index.js
@@ -2,7 +2,12 @@ const express = require("express");
 require("dotenv").config();
 const {graphqlHTTP} = require("express-graphql");
 const schema = require("./schema/schema");
+const DBConnect = require("./config/database");
 const app = express();
+
+//connect to mongodb
+DBConnect();
+
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === "development"
